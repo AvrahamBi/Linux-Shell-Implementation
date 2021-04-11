4e69721f58ab66163379c2c3be1cac0fa88ec12c
AvrahamBi <avrahambarilan@gmail.com>
2021-04-11 13:07:36 +0300

    only cd left

--- ex1.c
+++ ex1.c
@@ -39,32 +39,29 @@ char* getLastWord(char* str){
 void jobs(struct Command list[100], int size) {
     int i = 0;
     for(; i <= size; i++) {
-        if(list[i].childPid == 0 || waitpid(list[i].childPid, NULL, WNOHANG) != 0) {
-            list[i].isDone = 1;
-        } else {
-            list[i].isDone = 0; // todo maybe delete it
-            if(list[i].isInBackground == 1) {
-                printf("%s\n", list[i].commandName);
-                printf("jobs");
-                fflush(stdout);
-            }
+        if (list[i].isInBackground && !list[i].isDone) {
+            printf("%s\n", list[i].commandName);
         }
     }
+    list[size].isDone = 1;
 }
 
 void history(struct Command list[100], int size) {
     int i = 0;
+    int isLastCommand = 0;
+    char commandNameCopy[100];
     for(; i <= size; i++) {
-        if(list[i].childPid == 0 || waitpid(list[i].childPid, NULL, WNOHANG) != 0) {
-            printf("%s ", list[i].commandName);
-            printf("DONE\n");
-            fflush(stdout);
-        } else {
-            printf("%s ", list[i].commandName);
+        if (i == size) {isLastCommand = 1;}
+        strcpy(commandNameCopy, list[i].commandName);
+        if (((waitpid(list[i].childPid, NULL, WNOHANG) == 0) || isLastCommand) && !list[i].isDone ) {
+            printf("%s ", commandNameCopy);
             printf("RUNNING\n");
+        } else {
+            printf("%s ", commandNameCopy);
+            printf("DONE\n");
         }
     }
-    //printf("history RUNNING\n"); // todo fix the running/done history issue
+    list[size].isDone = 1;
 }
 
 void cd() {
@@ -77,7 +74,7 @@ int main() {
     int shellIsRunning = 1;
     char* firstWord;
     char input[100];
-    char copyOfInput[100]; // todo
+    char copyOfInput[100];
     int commandID = 0;
     pid_t pid;
 
@@ -92,18 +89,15 @@ int main() {
         } else {
             listOfCommands[commandID].isInBackground = 0;
         }
-        strcpy(copyOfInput, input); // todo
+        strcpy(copyOfInput, input);
         strcpy(listOfCommands[commandID].commandName, input);
         listOfCommands[commandID].isDone = 0;
         listOfCommands[commandID].childPid = 0;
         firstWord = getFirstWord(input);
+        // if command is a built in function
         if(strcmp(firstWord, "jobs") == 0  || strcmp(firstWord, "history") == 0
             || strcmp(firstWord, "cd") == 0 || strcmp(firstWord, "exit") == 0) {
             listOfCommands[commandID].isBuiltIn = 1;
-        }
-
-
-        if(listOfCommands[commandID].isBuiltIn == 1) {
             if(strcmp(firstWord, "jobs") == 0){
                 jobs(listOfCommands, commandID);
             }
@@ -116,22 +110,19 @@ int main() {
             if(strcmp(firstWord, "exit") == 0){
                 exitFromShell();
             }
-
-
-        } else if(listOfCommands[commandID].isBuiltIn == 0) {
+        // if command is not a built in function
+        } else {
             pid = fork();
             if (pid < 0) {
                 printf("fork failed\n");
                 fflush(stdout);
             }
-            listOfCommands[commandID].childPid = pid;
-
             // if its the parent
             if (pid > 0) {
+                listOfCommands[commandID].childPid = pid;
                 // if the command doesnt suppose to be in background
                 if(!listOfCommands[commandID].isInBackground) {
-                    //int waitStatus = waitpid(pid, NULL, 0);
-                    // wait to child
+                    // wait to child process
                     if(waitpid(pid, NULL, 0) == -1) { // todo maybe bug here
                         printf("An error occured\n");
                         exitFromShell();
@@ -139,7 +130,8 @@ int main() {
                 }
             }
             // if its child process
-            if(pid == 0) { // todo change to zero
+            if (pid == 0) {
+                // split the command into tokens
                 char delim[] = " ";
                 char *exeCommands[100];
                 int i = 0;
@@ -151,8 +143,8 @@ int main() {
                     exeCommands[i] = ptr;
                 }
                 exeCommands[i + 1] = NULL;
-                //int status = execvp(exeCommands[0], exeCommands);
-                if (execvp(exeCommands[0], exeCommands) == -1) {// todo maybe bug here
+                // execute the command
+                if (execvp(exeCommands[0], exeCommands) == -1) {
                     printf("exec failed\n");
                     fflush(stdout);
                     exitFromShell();
